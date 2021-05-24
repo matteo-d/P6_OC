6399ff0ad1029d316023c9382ac1766b93ea6dcf
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 00:40:11 +0200

    Début auth

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
+// CONNECT TO MONGO DB 
+mongoose
+  .connect(
+    "mongodb+srv://sopeckocko:MRkkLPHmXfmwSSIh@cluster0.ntwtw.mongodb.net/P6_OC?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+// CORS 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+//
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
@@ -20,5 +38,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
 });
+app.use(bodyParser.json());
+
+
 
 module.exports = app;

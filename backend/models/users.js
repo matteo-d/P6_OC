4c871eedef6cdf79de0d7dbe1397252f0380ba45
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 00:47:33 +0200

    feat: setup model user

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 
 const userSchema = mongoose.Schema({
@@ -6,7 +7,7 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true }
   });
   
-
+  userSchema.plugin(uniqueValidator);
   
   module.exports = mongoose.model('User', userSchema); 
   

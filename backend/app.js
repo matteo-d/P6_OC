e4ba639a3f5a1ecdc39f68ce4b1b99f0de62d69e
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 22:19:42 +0200

    CRUD sauce ok

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require ('mongoose')
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const sauceModel = require("./models/Sauce"); // Import le models de sauce
+const sauce = require("./models/Sauce");
 // CONNECT TO MONGO DB
 mongoose
   .connect(
@@ -10,8 +12,6 @@ mongoose
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-
 
 // CORS
 app.use((req, res, next) => {
@@ -27,37 +27,43 @@ app.use((req, res, next) => {
   next();
 });
 // MIDDLEWARE
-app.use(bodyParser.json()); // Transforme le corp de la requete en JSON pour toutes les routes 
+app.use(bodyParser.json()); // Transforme le corp de la requete en JSON pour toutes les routes
 
-// METTRE EN VENTE DES SAUCE (POST) ------------ !!! Attention ROUTES post avant Routes GET
-app.post('/api/sauces', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({ 
-    message: 'Sauce créé !'
+//  Routes Enregistrement des Sauces dans la base de données (POST) ------------ !!! Attention ROUTES post avant Routes GET
+app.post("/api/sauces", (req, res, next) => {
+  delete req.body._id;
+  const sauce = new Sauce({
+    ...req.body,
   });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+// Routes Récupération de la liste de Sauce en vente( GET )
+app.get("/api/sauces", (req, res, next) => {
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-
-// METTRE DES SAUCES EN VENTES ( GET )
-app.use("/api/sauces", (req, res, next) => {
-  const sauce = [
-    {
-      userId: "identifiant unique créé par MongoDB",
-      name: " nom de la sauce",
-      manufacturer: " fabricant de la sauce ",
-      description: " description de la sauce ",
-      mainPepper: " principal ingrédient dans la sauce",
-      imageUrl: "URL de l'image téléchargé par l'utilisateur",
-      heat: "MUST BE A NUMBER",
-      likes: "MUST BE A NUMBER",
-      dislikes: "MUST BE A NUMBER",
-      usersLiked: "tableau d'identifiants d'utilisateurs ayant aimé la sauce",
-      usersDisliked:
-        "tableau d'identifiants d'utilisateurs n'ayant pas aimé la sauce",
-    },
-  ];
-  res.status(200).json(sauce); // Si la réponse est de statut 200 (Réussite de la requete) mets l'objet sauce en JSON et display le dans l'API
+// Routes Récupération d'une sauce spécifique
+app.get("/api/sauces/:id", (req, res, next) => {
+  // LE ":id" indique a express que la route est dynamique
+  Sauce.findOne({ _id: req.params.id }) // Objet de comparaison en argument
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
 });
-
-
+// Routes Mettez à jour une sauce existante
+app.put("/api/stuff/:id", (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+// Route Suppression d'une sauce 
+app.delete('/api/sauces/:id', (req, res, next) => {
+ Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 module.exports = app;

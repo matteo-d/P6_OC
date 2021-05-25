cca7972dbc13bb096984e73eb1ed98968c728402
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 20:09:04 +0200

    Création route get sauce

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
-
-
-// MIDDLEWARE 
-app.use((req, res, next) => { // 
-  console.log('Requête reçue !');
-  next(); // Renvoie au prochain middleware 
-});
-
+// CORS
 app.use((req, res, next) => {
-  res.status(201); // Si le middleware d'avant marche, mettre le code 201 ( Requête réussi et ressource créer)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   next();
 });
+// MIDDLEWARE
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' }); // Si le middleware d'avant marche, dire que la reponse sera en format JSON et enverra a notre API la paire clé / valeur => message / texte
-  next();
-});
-
-app.use((req, res) => {
-  console.log('Réponse envoyée avec succès !'); // Si le middleware d'avant marche, console log
+// METTRE DES SAUCES EN VENTES
+app.use("/api/sauces", (req, res, next) => {
+  const sauce = [
+    {
+      userId: "identifiant unique créé par MongoDB",
+      name: " nom de la sauce",
+      manufacturer: " fabricant de la sauce ",
+      description: " description de la sauce ",
+      mainPepper: " principal ingrédient dans la sauce",
+      imageUrl: "URL de l'image téléchargé par l'utilisateur",
+      heat: "MUST BE A NUMBER",
+      likes: "MUST BE A NUMBER",
+      dislikes: "MUST BE A NUMBER",
+      usersLiked: "tableau d'identifiants d'utilisateurs ayant aimé la sauce",
+      usersDisliked:
+        "tableau d'identifiants d'utilisateurs n'ayant pas aimé la sauce",
+    },
+  ];
+  res.status(200).json(sauce); // Si la réponse est de statut 200 (Réussite de la requete) mets l'objet sauce en JSON et display le dans l'API
 });
 
 module.exports = app;

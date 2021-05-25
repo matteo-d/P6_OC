1d8d4278d41bdaa80d410ab495b1f0a8f55bddc9
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 18:12:06 +0200

    perdu save avant retour en arrière

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const app = express();
-const User = require("./models/User");
-const userRoutes = require("./routes/user");
+require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const path = require('path')
+var cors = require('cors')
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user')
+
+const app = express()
+
+
 // CONNECT TO MONGO DB
 mongoose
   .connect(
@@ -15,21 +22,21 @@ mongoose
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
-// BODY PARSER
-app.use(bodyParser.json());
 
-// ROUTE AUTH
-app.use("/api/auth", userRoutes);
+
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes)
+
 
 module.exports = app;

0c9b57076a6401f097ffd8c4e269f5566f143795
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-25 02:42:48 +0200

    avancé login et setup avant token d'auth

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
-const userRoutes = require('./routes/users.js');
-// CONNECT TO MONGO DB 
+const User = require("./models/User");
+const userRoutes = require("./routes/user");
+// CONNECT TO MONGO DB
 mongoose
   .connect(
     "mongodb+srv://sopeckocko:MRkkLPHmXfmwSSIh@cluster0.ntwtw.mongodb.net/P6_OC?retryWrites=true&w=majority",
@@ -12,27 +13,23 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-// CORS 
+// CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-
-// ROUTE POST SAUCE
-  app.post('/api/sauces', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
-    });
-  });
-// ROUTE AUTH 
-  app.use('/api/auth', userRoutes);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+// BODY PARSER
 app.use(bodyParser.json());
 
-
-
-
+// ROUTE AUTH
+app.use("/api/auth", userRoutes);
 
 module.exports = app;

74f6cc1f67ccf1871285097e9ddc3ec079ad308d
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-05-29 16:01:08 +0200

    origine beug trouvée : auth

--- backend/app.js
+++ backend/app.js
@@ -31,31 +31,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/sauces', (req, res, next) => {
-  const sauces = [
-    {
-      userId: 'Mon premier objet',
-      name: 'Mon premier objet',
-      manufacturer: 'Mon premier objet',
-      description: 'Mon premier objet',
-      mainPepper: 'Mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      heat:'Mon premier objet',
-
-    },
-    {
-      userId: 'Mon premier objet',
-      name: 'Mon premier objet',
-      manufacturer: 'Mon premier objet',
-      description: 'Mon premier objet',
-      mainPepper: 'Mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      heat:'Mon premier objet',
-
-    },
-  ];
-  res.status(200).json(sauces);
-});
 // BODY PARSER
 app.use(bodyParser.json()); // Transforme le corp de la requete en JSON pour toutes les routes
 app.use(cors());

e94d4f07594a9a2e7f8b9a7518f570a7aa8c5d4e
Mattéo DROUIN <66211519+matteo-d@users.noreply.github.com>
2021-07-08 21:24:09 +0200

    Add maskedEmail to DB

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,69 +6,78 @@ const TOKEN = process.env.TOKEN;
 const middlewareUser = require("../middleware/user");
 // Routes Récupération de la liste de Sauce en vente( GET )
 
+const maskEmail = (email) => {
+    let str = email;
+    str = str.split("");
+    let finalArr = [];
+    let len = str.indexOf("@");
+    str.forEach((item, pos) => {
+        pos >= 1 && pos <= len - 2
+            ? finalArr.push("*")
+            : finalArr.push(str[pos]);
+    });
+    let maskedEmail = finalArr.join("");
+    return maskedEmail;
+};
 
 exports.signup = (req, res, next) => {
-  // La fonction de hashage de bcrypt est asynchrone
-  if (middlewareUser.isValidPassword(req.body.password) && middlewareUser.isValidEmail(req.body.email)) {
-    bcrypt
-      .hash(req.body.password, 10) // Ici on hash 10x le mdp
-      .then((hash) => {
-        // On récupère le hash du mdp
-        const user = new User({
-          // Puis on créer un nouvel user
-          email: req.body.email, // On renregistre l'adresse qui est dans le corp de la requete ( ce que l'utilisateur a rentré )
-          password: hash, // Ici on enregistre bien directement le mdp crypté dans la DB et pas le mdp saisie
-        });
-        user
-          .save() // On save le nouvel user dans la DB
-          .then(() => res.status(201).json({ message: "Utilisateur créé !" })) // Réponse vers le frontend sinon dis que la requete n'est pas aboutie
-          .catch((error) => res.status(400).json({ error })); // Error 400 = Bad request
-      })
-      .catch((error) => res.status(500).json({ error })); // Error 500 = Error server
-  } else {
-    res
-      .status(401)
-      .json({
-        message:
-          "OUPS ! Votre mot de passe et/ou email est erroné ",
-      }); // Error 500 = Error server
-  }
+    // La fonction de hashage de bcrypt est asynchrone
+    if (
+        middlewareUser.isValidPassword(req.body.password) &&
+        middlewareUser.isValidEmail(req.body.email)
+    ) {
+        bcrypt
+            .hash(req.body.password, 10) // Ici on hash 10x le mdp
+            .then((hash) => {
+                // On récupère le hash du mdp
+                const user = new User({
+                    // Puis on créer un nouvel user
+                    email: req.body.email, // On renregistre l'adresse qui est dans le corp de la requete ( ce que l'utilisateur a rentré )
+                    password: hash, // Ici on enregistre bien directement le mdp crypté dans la DB et pas le mdp saisie
+                    maskedEmail: maskEmail(req.body.email)
+                });
+                user.save() // On save le nouvel user dans la DB
+                    .then(() =>
+                        res.status(201).json({ message: "Utilisateur créé !" })
+                    ) // Réponse vers le frontend sinon dis que la requete n'est pas aboutie
+                    .catch((error) => res.status(400).json({ error })); // Error 400 = Bad request
+            })
+            .catch((error) => res.status(500).json({ error })); // Error 500 = Error server
+    } else {
+        res.status(401).json({
+            message: "OUPS ! Votre mot de passe et/ou email est erroné ",
+        }); // Error 500 = Error server
+    }
 };
 
 exports.login = (req, res, next) => {
-  // Permet aux users existant de se connecter
-  User.findOne({ email: req.body.email }) // Cherche un seul utilisateur dont l'adresse mail correspond
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(401)
-          .json({
-            error:
-              "OUPS ! Mauvais identifiants de connexion, Veuillez Réessayer",
-          }); // Si pas de match error 401 = Unauthorized
-      }
-      bcrypt
-        .compare(req.body.password, user.password) // Fonction compare de bcrypt pour voir si mdp et hash matchs, renvoie boolean
-        .then((valid) => {
-          if (!valid) {
-            return res
-              .status(401)
-              .json({
-                error:
-                  "OUPS ! Mauvais identifiants de connexion, Veuillez Réessayer",
-              }); // Si pas de match error 401 = Unauthorized
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              // Fonction sign prends en param l'user Id , la clef de cryptage secrete
-              { userId: user._id }, // Payload, càd les données que l'on veut encoder
-              `${TOKEN}`, // Clef secrete (ou pas)
-              { expiresIn: "24h" } // Quand la clef secrete va exprirer
-            ),
-          });
+    // Permet aux users existant de se connecter
+    User.findOne({ email: req.body.email }) // Cherche un seul utilisateur dont l'adresse mail correspond
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: "OUPS ! Mauvais identifiants de connexion, Veuillez Réessayer",
+                }); // Si pas de match error 401 = Unauthorized
+            }
+            bcrypt
+                .compare(req.body.password, user.password) // Fonction compare de bcrypt pour voir si mdp et hash matchs, renvoie boolean
+                .then((valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: "OUPS ! Mauvais identifiants de connexion, Veuillez Réessayer",
+                        }); // Si pas de match error 401 = Unauthorized
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            // Fonction sign prends en param l'user Id , la clef de cryptage secrete
+                            { userId: user._id }, // Payload, càd les données que l'on veut encoder
+                            `${TOKEN}`, // Clef secrete (ou pas)
+                            { expiresIn: "24h" } // Quand la clef secrete va exprirer
+                        ),
+                    });
+                })
+                .catch((error) => res.status(500).json({ error })); // Error 500 = Error server
         })
-        .catch((error) => res.status(500).json({ error })); // Error 500 = Error server
-    })
-    .catch((error) => res.status(500).json({ error }));
+        .catch((error) => res.status(500).json({ error }));
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator"); // Package pour MongoDB pour vérifier que e-mail est unique , marche mieux que " required: true";
 
 const userSchema = mongoose.Schema({
-  email: {  type: String,
-    required: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-    unique: true },
+    email: {
+        type: String,
+        required: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+        unique: true,
+    },
 
-  password: { type: String,
-    required: true ,
-    
-    }, // Meme si le MDP est crypté il est de type string
+    password: { type: String, required: true }, // Meme si le MDP est crypté il est de type string
+    maskedEmail: { type: String }
 });
 
 userSchema.plugin(uniqueValidator);
